1f9436a7f3471d8f226ac8e722c0b8623acaf829
txag1981 <txag1981@gmail.com>
2017-05-19 09:49:12 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProject1
+//
+//  Created by Clinton Scott on 5/19/17.
+//  Copyright © 2017 Clinton Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
